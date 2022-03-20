995feadeab4692e3a213e9845c7f2ad5ef7225d0
Franklin Shera <fshera96@gmail.com>
2022-03-20 15:22:58 +0300

    Hero Section Responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,31 @@
 <x-layout>
-    <section class="hero" id="hero">
+    <div class="home-page">
+        <section class="hero" id="hero">
 
-        <div class=" mrsmart-container hero-content">
-            <div class="welcome-container">
-                <span class="welcome">Welcome to</span>
+            <div class=" mrsmart-container hero-content">
+                <div class="welcome-container">
+                    <span class="welcome">Welcome to</span>
+                </div>
+
+                <h1>MrSmart Cleaning <br> Services</h1>
+
+                <p class="slogan">Transforming your locality to a clean, healthy living<br>
+                    and working environment.</p>
+
+
+
             </div>
 
-            <h1>MrSmart Cleaning <br> Services</h1>
-
-            <p class="slogan">Transforming your locality to a clean, healthy living<br>
-                and working environment.</p>
+            <img src="storage/images/home.webp" alt="Home hero Image">
 
 
+        </section>
+        <section class="mrsmart-container services" id="services">
 
-        </div>
+        </section>
 
-        <img src="storage/images/home.webp" alt="Home hero Image">
+        <section class="contact" id="contact">
 
-
-    </section>
-    <section class="mrsmart-container services" id="services">
-
-    </section>
-
-    <section class="contact" id="contact">
-
-    </section>
+        </section>
+    </div>
 </x-layout>
