5907cadc0ce5c7d6b0eca405bbe109e744c0c636
Franklin Shera <fshera96@gmail.com>
2022-03-22 00:53:26 +0300

    404 Page Responsive

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="not-found-page">
-        <h1>Ooops</h1>
+        <h1>Ooops!</h1>
         <p>Page Not Found</p>
         <a href="{{ route('welcome') }}">Lets Get Home</a>
     </div>
